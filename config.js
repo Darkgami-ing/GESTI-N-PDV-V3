0ba6910ef8748dbb94c29444b365c7e2bef32cc8
Darkgami-ing <magnios2010@gmail.com>
2026-09-20 12:39:20 -0500

    Configurar endpoint de evidencias en Google Drive

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = Object.freeze({
   SUPABASE_URL: "https://qtgcpmpbtfhvoasncmkg.supabase.co",
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_tj-kyp78upcwyrLbhMTqaA_l3lfRpt8",
-  DRIVE_API_URL: "",
+  DRIVE_API_URL: "https://script.google.com/macros/s/AKfycbxe4pJaQyyIT7lYFlTGulWwAsZ3mlH-owAzs5kZe3uLtEi_aS5HWeUArHbKizzTslQX/exec",
   INTERNAL_EMAIL_DOMAIN: "control-logistico.local",
   TIME_ZONE: "America/Lima",
   MAX_GENERAL_PHOTOS: 3,
